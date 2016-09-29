f4ffe51dda43c4a6b80cd6d39dd97aae3b89f939
avarne <avarne@Digitelt91.digite.in>
2016-09-29 14:06:58 +0530

    Test

--- app.js
+++ app.js
@@ -32,4 +32,5 @@ app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+  console.log('Anand..');
 });
